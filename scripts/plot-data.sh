d83e25e0bc54f3f53f2c88a85df334fc8e08801d
Ryan Karl <brightycloud@gmail.com>
2019-07-02 00:13:49 +0000

    Modify gnuplot script as well as shell script to make graphs clearer and easier to read.

--- scripts/plot-data.sh
+++ scripts/plot-data.sh
@@ -3,6 +3,7 @@ while [ true ];do
   DBFILE=`ls -1t data/ | head -n 1`
   for HRS in 1 6 24 144 720;do 
     ./gnuplot-data.py $[HRS*720] "data/$DBFILE" | (cat > /dev/shm/gnudata && trap 'rm /dev/shm/gnudata' EXIT && gnuplot -c plotcmds)
+    mogrify -negate plot.png
     mv plot.png `printf "plot-%03d.png" $HRS`
   done
   echo "Updated plots at $(date)"
